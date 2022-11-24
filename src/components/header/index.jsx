5e0f242f2df43f067368214f9f01238860954b7b
Clinton Rocha <clintongoncalvesrocha@gmail.com>
2022-11-24 20:56:08 -0300

    header finalizado

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,28 +1,44 @@
 import styles from "./styles.module.scss";
 import { useState } from "react";
+import iconsDown from "../../assets/iconDown.png";
+import iconsUp from "../../assets/iconUp.png";
+import blizzard from "../../assets/logo-blizzard.png";
+import iconLogar from "../../assets/logar-icon.png";
 
 export function Header() {
-    let [Arrow, Newarrow] = useState();
+    let [arrowJogo, NewarrowJogo] = useState(iconsDown);
 
-    function Arrowclick() {
-        if (Arrow === '') {
-            Arrow = '';
+    function ArrowclickJogo() {
+        if (arrowJogo === iconsDown) {
+            arrowJogo = iconsUp;
+        } else {
+            arrowJogo = iconsDown;
         }
-        Newarrow(Arrow);
+        NewarrowJogo(arrowJogo);
+    }
+    let [arrowEsport, NewarrowEsport] = useState(iconsDown);
+
+    function ArrowclickEsport() {
+        if (arrowEsport === iconsDown) {
+            arrowEsport = iconsUp;
+        } else {
+            arrowEsport = iconsDown;
+        }
+        NewarrowEsport(arrowEsport);
     }
     return (
         <>
             <header className={styles.header}>
-                <div>
-                    <img src="" alt="" />
-                </div>
+                <div className={styles.warrapImg}>
+                    <img className={styles.logoImg} src={blizzard} alt="icone da blizzard" />
                 <ul>
-                    <li onClick={Arrowclick}>
+                    <li onClick={ArrowclickJogo}>
                         <a href="#">Jogos</a>
-                        <img src={Arrow} alt="arrow" />
+                        <img src={arrowJogo} alt="arrow" />
                     </li>
-                    <li>
+                    <li onClick={ArrowclickEsport}>
                         <a href="#">Esports</a>
+                        <img src={arrowEsport} alt="arrow" />
                     </li>
                     <li>
                         <a href="#">Loja</a>
@@ -34,6 +50,17 @@ export function Header() {
                         <a href="#">Suporte</a>
                     </li>
                 </ul>
+                </div>
+                <div className={styles.warrapButton}>
+                    <button className={styles.criarContaBtn}>Crir conta</button>
+                    <button className={styles.logarBtn}>
+                        <img
+                            src={iconLogar}
+                            alt="icone do botão para entrar na conta"
+                        />
+                        Logar
+                    </button>
+                </div>
             </header>
         </>
     );

931bafb59be92f155820506d0759d9b2334e9839
Clinton Rocha <clintongoncalvesrocha@gmail.com>
2022-11-25 04:15:52 -0300

    iniciando o banner

---
+++ src/components/main/index.jsx
@@ -0,0 +1,20 @@
+const mainContent = [{}]
+export function Main() {
+    return (
+        <>
+            <main>
+                <aside>
+                    <button></button>
+                </aside>
+                <div>
+                    <h1>Retorne à escuridão com o game Diablo IV</h1>
+                    <p>O retorno de Lilith traz uma era de escuridão e sofrimento</p>
+                    <button></button>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </main>
+        </>
+    );
+}

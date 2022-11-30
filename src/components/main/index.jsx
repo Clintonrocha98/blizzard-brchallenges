5bbae05f1bbc288fdcdaa5c304ebdbbdb8973c86
Clinton Rocha <clintongoncalvesrocha@gmail.com>
2022-11-30 00:26:18 -0300

    sass up

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,5 +1,5 @@
 import gameData from "../gameData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
 export function Main({ className, children }) {
     const [gameContent, attGame] = useState(gameData[0]);
@@ -39,19 +39,14 @@ export function Main({ className, children }) {
             <main className={className}>
                 <div className={styles.warrapMain}>
                     <aside className={styles.left}>
-                        <select>
-                            {gameData.map((game) => (
-                                <option
-                                    name="gameIcon"
-                                    src={game.icon[1]}
-                                    key={game.id}
-                                    alt="icone do jogo"
-                                    onClick={(e) =>
-                                        handleIcon(e, game, game.id)
-                                    }
-                                ></option>
-                            ))}
-                        </select>
+                        {gameData.map((game) => (
+                            <img
+                                src={game.icon[1]}
+                                key={game.id}
+                                alt="icone do jogo"
+                                onClick={(e) => handleIcon(e, game, game.id)}
+                            />
+                        ))}
                     </aside>
                     <div className={styles.warrapTitle}>
                         <h1>{gameContent.title}</h1>
@@ -59,14 +54,14 @@ export function Main({ className, children }) {
                         <button>{gameContent.btntext}</button>
                     </div>
                     <aside>
-                        <div>
+                        <div className={styles.logo}>
                             <img
                                 src={gameContent.logo}
                                 alt={gameContent.name + " logo"}
                                 key={gameContent.id}
                             />
                         </div>
-                        <div>
+                        <div className={styles.trailer}>
                             <p>assista o trailer</p>
                             <img
                                 onMouseEnter={handleEnter}
